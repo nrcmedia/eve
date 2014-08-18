39df7e65dc1d75d7153b550cf0f9172e3d76ad28
Eelke Hermens <eelkehermens@gmail.com>
2014-08-18 16:39:12 +0200

    minimal timezone support for queries

--- eve/routes.py
+++ eve/routes.py
@@ -124,7 +124,7 @@ class ApiView(MethodView):
         embedded resources. Used by POST and PUT requests """
 
         if not request.is_json:
-            abort(400, 'No valid Content-Type')
+            abort(400, 'Not a valid Content-Type')
 
         payload = request.get_json()
         doc = self._parse(payload)
@@ -679,6 +679,7 @@ def _prep_query(query):
 
     def convert_objects(q):
         for key, val in q.iteritems():
+            print val
             if isinstance(val, dict):
                 q[key] = convert_objects(val)
             #elif isinstance(val, list):
@@ -686,11 +687,16 @@ def _prep_query(query):
             elif isinstance(val, basestring) and re.match(r"^[0-9a-fA-F]{24}$", val):
                 # @TODO This also matches strings that look like object id's
                 q[key] = ObjectId(val)
-            elif isinstance(val, basetring) and re.match(r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}", val):
-                q[key] = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S")
-            # @TODO explicit timezone handling
-            elif isinstance(val, basestring) and re.match(r"\d{4}-\d{2}-\d{2}", val):
+            elif isinstance(val, basestring) and re.match(r"^\d{4}-\d{2}-\d{2}$", val):
                 q[key] = datetime.strptime(val, "%Y-%m-%d")
+            elif isinstance(val, basestring) and re.match(r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$", val):
+                q[key] = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S")
+            elif isinstance(val, basestring) and re.match(r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\w$", val):
+                # explicit timezone handling
+                # datetime tz handling is not platform independent, should use other package, val = val.replace(' ', '+')
+                # @TODO for now only support UTC/Z flags for tz
+                if val[-1] == 'Z': val = val[:-1] + 'UTC'
+                q[key] = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S%Z")
         return q
 
     return convert_objects(query)
@@ -699,6 +705,7 @@ def get_context():
     """ Retreive the URL parameters from the current request context """
 
     args = request.args
+    print args
     int_or_none = lambda x: int(x) if x is not None else None
     dict_or_none = lambda x: json.loads(x) if x is not None else None
 
